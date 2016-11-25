52aaca6a63f2f58143c7cd4411e67009d566d21b
Matty Wong-Rose <mathieu.wong-rose@pwgsc.gc.ca>
2016-11-25 15:13:13 -0800

    Add project files.

---
+++ egdBooking_v2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(egdBooking_v2.Startup))]
+namespace egdBooking_v2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
