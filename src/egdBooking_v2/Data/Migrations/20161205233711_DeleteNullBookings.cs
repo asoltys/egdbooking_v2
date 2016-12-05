7f145270e41f1ed5da5d58ac8e1e0feb6bb0081f
Adam Soltys <asoltys@gmail.com>
2016-12-05 15:39:54 -0800

    Delete bookings with null Vessel

---
+++ src/egdBooking_v2/Data/Migrations/20161205233711_DeleteNullBookings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace egdbooking_v2.Data.Migrations
+{
+    public partial class DeleteNullBookings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+          migrationBuilder.Sql(@"
+              DELETE FROM Bookings WHERE VesselID IS NULL;
+          ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
